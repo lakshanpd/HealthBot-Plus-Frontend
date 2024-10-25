7a10ad7f3fba23e967017c79b12996efeb11cc10
Danuka Lakshan <lakshanpd.cse21@gmail.com>
2024-10-25 19:46:50 +0530

    updated gettingStarted youtube vedio

--- src/pages/gettingStarted.jsx
+++ src/pages/gettingStarted.jsx
@@ -1,33 +1,67 @@
 import React from "react";
-import { FaUserPlus, FaSignInAlt, FaGoogle, FaRobot, FaCheckCircle } from "react-icons/fa";
+import {
+  FaUserPlus,
+  FaSignInAlt,
+  FaGoogle,
+  FaRobot,
+  FaCheckCircle,
+} from "react-icons/fa";
 import NavbarGettingStarted from "../components/navbar_gettingStarted";
 import Footer from "../components/footer";
 
 export default function GettingStarted() {
   return (
     <div>
-      <NavbarGettingStarted /> {/* Custom Navbar */}
+      <NavbarGettingStarted />
+
+      {/* Video Section*/}
+      <section className="bg-gray-100 py-10">
+        <div className="container mx-auto text-center">
+          <h2 className="text-4xl font-bold text-blue-600 mb-6">
+            Watch a Demo of HealthBot+
+          </h2>
+          <div className="mt-8 flex justify-center">
+            <div className="shadow-lg rounded-lg overflow-hidden">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/GAosly8RCoQ?si=gACGEXRu5DayvXXJ"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
+        </div>
+      </section>
 
       {/* Introduction Section */}
       <section className="bg-gray-100 py-10">
         <div className="container mx-auto text-center">
-          <h1 className="text-4xl font-bold text-blue-600 mb-6">Getting Started with HealthBot+</h1>
+          <h1 className="text-4xl font-bold text-blue-600 mb-6">
+            Getting Started with HealthBot+
+          </h1>
           <p className="text-lg text-gray-600 mb-4">
-            Follow these steps to begin your journey with HealthBot+. We’ll guide you through creating an account, logging in, and accessing your personalized skin health services.
+            Follow these steps to begin your journey with HealthBot+. We’ll
+            guide you through creating an account, logging in, and accessing
+            your personalized skin health services.
           </p>
         </div>
       </section>
 
       {/* Vertical Timeline Steps */}
       <section className="py-10">
-        <div className="container mx-auto flex flex-col space-y-10 pl-4"> {/* Adjusted padding to leave space for numbers */}
-          
+        <div className="container mx-auto flex flex-col space-y-10 pl-4">
+          {" "}
+          {/* Adjusted padding to leave space for numbers */}
           {/* Step 1 */}
           <div className="relative flex items-center">
             <div className="flex items-center justify-center w-10 h-10 rounded-full bg-blue-600 text-white font-bold absolute -left-12">
               1
             </div>
-            <div className="flex-1 bg-white border-l-4 border-blue-600 rounded-lg p-6 shadow-lg flex items-center"> 
+            <div className="flex-1 bg-white border-l-4 border-blue-600 rounded-lg p-6 shadow-lg flex items-center">
               <div className="flex-shrink-0 mr-6">
                 <img
                   src="../images/LoginSignUp.png"
@@ -37,12 +71,16 @@ export default function GettingStarted() {
               </div>
               <div>
                 <FaUserPlus className="text-blue-500 text-5xl mb-4" />
-                <h2 className="text-2xl font-semibold text-gray-800 mb-2">Step 1: Click the Login/SignUp Button</h2>
-                <p className="text-gray-600">At the top right corner of the page, click the Login/SignUp button to begin.</p>
+                <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+                  Step 1: Click the Login/SignUp Button
+                </h2>
+                <p className="text-gray-600">
+                  At the top right corner of the page, click the Login/SignUp
+                  button to begin.
+                </p>
               </div>
             </div>
           </div>
-
           {/* Step 2 */}
           <div className="relative flex items-center bg-gray-50">
             <div className="flex items-center justify-center w-10 h-10 rounded-full bg-blue-600 text-white font-bold absolute -left-12">
@@ -58,12 +96,16 @@ export default function GettingStarted() {
               </div>
               <div>
                 <FaSignInAlt className="text-green-500 text-5xl mb-4" />
-                <h2 className="text-2xl font-semibold text-gray-800 mb-2">Step 2: Signing Up</h2>
-                <p className="text-gray-600">If you don’t have an account yet, click the <strong>SignUp</strong> button to proceed.</p>
+                <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+                  Step 2: Signing Up
+                </h2>
+                <p className="text-gray-600">
+                  If you don’t have an account yet, click the{" "}
+                  <strong>SignUp</strong> button to proceed.
+                </p>
               </div>
             </div>
           </div>
-
           {/* Step 2.1 */}
           <div className="relative flex items-center">
             <div className="flex items-center justify-center w-10 h-10 rounded-full bg-blue-600 text-white font-bold absolute -left-12">
@@ -79,14 +121,18 @@ export default function GettingStarted() {
               </div>
               <div>
                 <FaCheckCircle className="text-purple-500 text-5xl mb-4" />
-                <h2 className="text-2xl font-semibold text-gray-800 mb-2">Step 2.1: Fill Your Details</h2>
+                <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+                  Step 2.1: Fill Your Details
+                </h2>
                 <p className="text-gray-600">
-                  Enter your <strong>Name</strong>, <strong>Country</strong>, <strong>Birthday</strong>, <strong>Sex</strong>, and <strong>Description</strong>. Then, click <strong>Next</strong>.
+                  Enter your <strong>Name</strong>, <strong>Country</strong>,{" "}
+                  <strong>Birthday</strong>, <strong>Sex</strong>, and{" "}
+                  <strong>Description</strong>. Then, click{" "}
+                  <strong>Next</strong>.
                 </p>
               </div>
             </div>
           </div>
-
           {/* Step 2.2 */}
           <div className="relative flex items-center bg-gray-50">
             <div className="flex items-center justify-center w-10 h-10 rounded-full bg-blue-600 text-white font-bold absolute -left-12">
@@ -102,17 +148,17 @@ export default function GettingStarted() {
               </div>
               <div>
                 <FaUserPlus className="text-blue-500 text-5xl mb-4" />
-                <h2 className="text-2xl font-semibold text-gray-800 mb-2">Step 2.2: Create Your Account</h2>
+                <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+                  Step 2.2: Create Your Account
+                </h2>
                 <p className="text-gray-600">
-                  Enter your <strong>Email</strong>, <strong>Password</strong>, and <strong>Confirm Password</strong>. Then, select the doctor from the list and click the <strong>SignUp</strong> button.
+                  Enter your <strong>Email</strong>, <strong>Password</strong>,
+                  and <strong>Confirm Password</strong>. Then, select the doctor
+                  from the list and click the <strong>SignUp</strong> button.
                 </p>
               </div>
             </div>
           </div>
-
-
-
-
           {/* Step 3 */}
           <div className="relative flex items-center">
             <div className="flex items-center justify-center w-10 h-10 rounded-full bg-blue-600 text-white font-bold absolute -left-12">
@@ -128,14 +174,16 @@ export default function GettingStarted() {
               </div>
               <div>
                 <FaSignInAlt className="text-green-500 text-5xl mb-4" />
-                <h2 className="text-2xl font-semibold text-gray-800 mb-2">Step 3: Click Log In</h2>
+                <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+                  Step 3: Click Log In
+                </h2>
                 <p className="text-gray-600">
-                  If you already have an account, press the <strong>Log In</strong> button to proceed.
+                  If you already have an account, press the{" "}
+                  <strong>Log In</strong> button to proceed.
                 </p>
               </div>
             </div>
           </div>
-
           {/* Step 3.1 */}
           <div className="relative flex items-center bg-gray-50">
             <div className="flex items-center justify-center w-10 h-10 rounded-full bg-blue-600 text-white font-bold absolute -left-12">
@@ -151,14 +199,17 @@ export default function GettingStarted() {
               </div>
               <div>
                 <FaSignInAlt className="text-green-500 text-5xl mb-4" />
-                <h2 className="text-2xl font-semibold text-gray-800 mb-2">Step 3.1: Email & Password</h2>
+                <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+                  Step 3.1: Email & Password
+                </h2>
                 <p className="text-gray-600">
-                  Enter your <strong>Email</strong> and <strong>Password</strong>, then click <strong>Login</strong> to access your account.
+                  Enter your <strong>Email</strong> and{" "}
+                  <strong>Password</strong>, then click <strong>Login</strong>{" "}
+                  to access your account.
                 </p>
               </div>
             </div>
           </div>
-
           {/* Step 3.2 */}
           <div className="relative flex items-center">
             <div className="flex items-center justify-center w-10 h-10 rounded-full bg-blue-600 text-white font-bold absolute -left-12">
@@ -174,15 +225,18 @@ export default function GettingStarted() {
               </div>
               <div>
                 <FaGoogle className="text-red-500 text-5xl mb-4" />
-                <h2 className="text-2xl font-semibold text-gray-800 mb-2">Step 3.2: Google Authentication</h2>
+                <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+                  Step 3.2: Google Authentication
+                </h2>
                 <p className="text-gray-600">
-                Alternatively, you can <strong>seamlessly log in</strong> using <strong>Google Authentication</strong> by clicking the <strong>Continue with Google</strong> button for a fast and secure login experience.
-
+                  Alternatively, you can <strong>seamlessly log in</strong>{" "}
+                  using <strong>Google Authentication</strong> by clicking the{" "}
+                  <strong>Continue with Google</strong> button for a fast and
+                  secure login experience.
                 </p>
               </div>
             </div>
           </div>
-
           {/* Step 4 */}
           <div className="relative flex items-center">
             <div className="flex items-center justify-center w-10 h-10 rounded-full bg-blue-600 text-white font-bold absolute -left-12">
@@ -198,26 +252,34 @@ export default function GettingStarted() {
               </div>
               <div>
                 <FaRobot className="text-blue-500 text-5xl mb-4" />
-                <h2 className="text-2xl font-semibold text-gray-800 mb-2">Step 4: Try HealthBot</h2>
+                <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+                  Step 4: Try HealthBot
+                </h2>
                 <p className="text-gray-600">
-                  Once logged in, press the <strong>Try HealthBot</strong> button to begin your personalized skin health journey with HealthBot+.
+                  Once logged in, press the <strong>Try HealthBot</strong>{" "}
+                  button to begin your personalized skin health journey with
+                  HealthBot+.
                 </p>
               </div>
             </div>
           </div>
-
         </div>
       </section>
 
       {/* Conclusion */}
       <section className="bg-gray-100 py-10">
         <div className="container mx-auto text-center">
-          <h2 className="text-2xl font-semibold text-blue-600 mb-6">You're All Set!</h2>
+          <h2 className="text-2xl font-semibold text-blue-600 mb-6">
+            You're All Set!
+          </h2>
           <p className="text-lg text-gray-600">
-            You’ve successfully created your account or logged in. Now, you can explore the powerful features of HealthBot+ to monitor and take care of your skin health.
+            You’ve successfully created your account or logged in. Now, you can
+            explore the powerful features of HealthBot+ to monitor and take care
+            of your skin health.
           </p>
         </div>
       </section>
+
       <Footer />
     </div>
   );
